27e69e87da95dc6057586815b0ba3001f86f5a4c
freek100 <75321447+freek100@users.noreply.github.com>
2020-12-05 15:01:32 +0100

    Update day5binary.py

--- day 5/day5binary.py
+++ day 5/day5binary.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec  5 14:13:31 2020
-
-@author: freek
-"""
-
 def readAndFormatData(filename):
     data= open(filename, 'r').read().split('\n')
     for line in data:
@@ -12,11 +5,9 @@ def readAndFormatData(filename):
     return data
 
 def dec2binary(line,one,zero):
-    line=line.replace(one,'1').replace(zero,'0')
-    result=0; base= 0 #Initializing values
+    line=line.replace(one,'1').replace(zero,'0'); result=0; base= 0 #Initializing values
     for val in line[::-1]:
-        result+= int(val)*2**base
-        base+=1
+        result+= int(val)*2**base; base+=1
     return result
 
 def seatID(passes):
@@ -25,14 +16,10 @@ def seatID(passes):
         seatIDs[passes.index(line)]=8*dec2binary(line[0],'F','B')+dec2binary(line[1],'R','L')
     return seatIDs
 
-def checkMissingID(seatIDs):
-    checklist= list(range(min(seatIDs)+8,max(seatIDs)-8)) #Plus and minus 8 as we're not on the first or last row
-    return list(set(checklist)-set(seatIDs))[0]
-
 def main():
     passes= readAndFormatData('day5.txt')
     seatIDs= seatID(passes)
     print(f'Part 1) Highest seat ID is {max(seatIDs)}')
-    print(f'Part 2) Your seat ID is {checkMissingID(seatIDs)}')
+    print(f'Part 2) Your seat ID is {list(set(list(range(min(seatIDs)+8,max(seatIDs)-8)))-set(seatIDs))[0]}')
     
 main()
